19abfebe8edcdb03f213579d7b26c8770ab81bc4
JuliaPasquier <pasquierjulia5@gmail.com>
2023-09-20 13:26:20 +0200

    animals

---
+++ exoJava.js
@@ -0,0 +1,9 @@
+const name = prompt("What is your name?");
+alert(`Welcome ! ${name}`);
+
+const animals = prompt("do you prefere dogs or cats?" );
+if (animals== "dogs") {
+    alert(`you are right,${animals}are our best friends !`);}
+else if(animals == "cats") {
+    alert(`I agree the,${animals}are so cute!`);
+}
